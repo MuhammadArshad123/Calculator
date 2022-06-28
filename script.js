562feb95288b3fce2f00a2b69ab0a02249b8adf2
MuhammadArshad123 <m.mannan.arshad@gmail.com>
2022-06-27 19:26:37 -0500

    Completed CSS Grid Layout, added Operation function to JS

---
+++ script.js
@@ -0,0 +1,16 @@
+const collection = document.getElementsByTagName("button");
+
+function operate(operation, a, b) {
+    switch (operation) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
+}
+
+
